5b6b4d185675d2c42f134e7c138c502d4e033098
Krzysztof Langner <klangner.mbank@gmail.com>
2015-12-03 15:20:18 +0100

    Sort by Longitude and then group as many as possible and sort by distance from NORTH

--- script/main.py
+++ script/main.py
@@ -12,31 +12,25 @@ def load_data():
     return pd.read_csv('../data/gifts.csv')
 
 
+def get_distance(item):
+    return item[1]
+
+
 def find_trips(df):
-    df['taken'] = False
+    df = df.sort(['Longitude'])
     trips = []
-    for i, row in df.iterrows():
-        if not row.taken:
-            # gifts, df = add_gifts(df, santa.NORTH_POLE, santa.WEIGHT_LIMIT-santa.SLEIGH_WEIGHT)
-            trips.append([1])#gifts)
-    return to_pandas(trips)
-
-
-def add_gifts(df, start_pos, available_weight):
     gifts = []
-    min_distance = 10**9
-    index = -1
+    weight = 0
     for i, row in df.iterrows():
-        if not row.taken and row.Weight < available_weight:
-            pos = row['Latitude'], row['Longitude']
-            dist = santa.haversine(start_pos, pos)
-            if dist < min_distance:
-                index = i
-                min_distance = dist
-    if index >= 0:
-        gifts.append(df.loc[[index]])
-        df.set_value(index, 'taken', True)
-    return gifts, df
+        if weight + row.Weight > santa.WEIGHT_LIMIT - santa.SLEIGH_WEIGHT:
+            trips.append(gifts)
+            gifts = []
+            weight = 0
+        weight += row.Weight
+        gifts.append((row.GiftId, santa.haversine((row.Latitude, row.Longitude), santa.NORTH_POLE)))
+    trips.append(gifts)
+    xs = [sorted(trip, key=get_distance) for trip in trips]
+    return to_pandas(xs)
 
 
 def save_solution(df):
@@ -49,8 +43,8 @@ def to_pandas(trips):
     with open('../data/solution.csv', "a") as f:
         f.write('GiftId,TripId')
         for trip_id, gifts in enumerate(trips):
-            for gift_id in gifts:
-                gift_series.append(gift_id)
+            for (gift_id, _) in gifts:
+                gift_series.append(int(gift_id))
                 trip_series.append(trip_id)
     return pd.DataFrame({'GiftId': gift_series, 'TripId': trip_series})
 
